107c440fb7e2eb82182e3d947fb467b1cf71718b
lmccraw123 <laurenmccraw@gmail.com>
2018-12-02 22:17:32 -0600

    adding routing module

---
+++ laurenMccraw/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
